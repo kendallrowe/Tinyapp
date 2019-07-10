e0360a5658af2b24cc85b65be3d3f713778023a3
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-10 21:38:09 +0000

    Linted

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,41 +4,41 @@ const { getUserByEmail, urlsForUser, validateUser } = require('../helpers.js');
 
 const testURLS = {
   "CaCft": {
-    longURL: "http://vicandkentietheknot.com/Home/", 
+    longURL: "http://vicandkentietheknot.com/Home/",
     userID: "userRandomID"
   },
   "adsf32": {
-    longURL: "https://www.lighthouselabs.ca", 
+    longURL: "https://www.lighthouselabs.ca",
     userID: "userRandomID2"
   },
   "ters44": {
-    longURL: "https://www.facebook.com/", 
+    longURL: "https://www.facebook.com/",
     userID: "userRandomID2"
   }
 };
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     assert.strictEqual(user, expectedOutput);
   });
 
   it("should return undefined if email isn't in database", function() {
-    const user = getUserByEmail("hello", testUsers)
+    const user = getUserByEmail("hello", testUsers);
     const expectedOutput = undefined;
     assert.strictEqual(user, expectedOutput);
   });
@@ -46,13 +46,13 @@ describe('getUserByEmail', function() {
 
 describe("urlsForUser", function() {
   it("should return array of correct shortURL and LongURLS in array if user has multiple urls on site", function() {
-    const user = urlsForUser("userRandomID2", testURLS)
+    const user = urlsForUser("userRandomID2", testURLS);
     const expectedOutput = [{shortURL: "adsf32", longURL: "https://www.lighthouselabs.ca"}, {shortURL: "ters44", longURL: "https://www.facebook.com/"}];
     assert.deepEqual(user, expectedOutput);
   });
 
   it("should return empty array if user has not created any urls", function() {
-    const user = urlsForUser("kendall", testURLS)
+    const user = urlsForUser("kendall", testURLS);
     const expectedOutput = [];
     assert.deepEqual(user, expectedOutput);
   });
@@ -61,19 +61,19 @@ describe("urlsForUser", function() {
 
 describe("validateUser", function() {
   it("should return true if shortURL exists in url database and has tag matching to current user", function() {
-    const user = validateUser("userRandomID2", "ters44", testURLS)
+    const user = validateUser("userRandomID2", "ters44", testURLS);
     const expectedOutput = true;
     assert.strictEqual(user, expectedOutput);
   });
 
   it("should return false if shortURL does not exists in url database", function() {
-    const user = validateUser("userRandomID2", "iewiuriu", testURLS)
+    const user = validateUser("userRandomID2", "iewiuriu", testURLS);
     const expectedOutput = false;
     assert.strictEqual(user, expectedOutput);
   });
 
   it("should return false if shortURL does exist in url database but to a different user", function() {
-    const user = validateUser("userRandomID2", "CaCft", testURLS)
+    const user = validateUser("userRandomID2", "CaCft", testURLS);
     const expectedOutput = false;
     assert.strictEqual(user, expectedOutput);
   });
